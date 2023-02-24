33e23c7e49700aa75257b5e91748cdd1bb595294
marcelojlrodrigues <marcelorodrigues_2005@hotmail.com>
2023-02-24 15:40:19 -0300

    Atualizacao ejs, css, json categorias

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const indexRoute = require('./routes/indexRoute');
 
-
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,7 @@
+const categorias = require('../models/categorias.json')
+
 const controller = {
-    index: (req, res) => res.render('index'),
+    index: (req, res) => res.render('index', { categorias }),
 }
 
 module.exports = controller;
