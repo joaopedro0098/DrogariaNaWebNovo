a7e024dda88a5f32227ed63653408dee560348f0
Daniel Batista <daniel.batistaguiar@gmail.com>
2023-02-26 22:29:04 -0300

    Feat: Começo HTML listagem de produtos
    HTMLl inicial da listagem de produtos, rodas, e algumas imagens

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const indexRoute = require('./routes/indexRoute');
 const contatoRoute = require('./routes/contatoRoute');
+const medicamentos = require('./routes/medicamentos');
 
 const app = express();
 
@@ -11,7 +12,8 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use('/', indexRoute);
-app.use('/contato', contatoRoute)
+app.use('/contato', contatoRoute);
+app.use('/medicamentos', medicamentos);
 
 app.listen(80, () => console.log("Servidor funcionando na porta 80"));
 
